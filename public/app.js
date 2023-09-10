ce208de6d7e1d3c6cdd45667cd5f2021a54811da
Iamneo <techadmin@iamneo.ai>
2023-09-10 11:30:33 +0000

    Backup Commit

--- public/app.js
+++ public/app.js
@@ -1,64 +1,79 @@
-// Initial game state
-let cells = ['', '', '', '', '', '', '', '', ''];
+// JavaScript code for the Tic Tac Toe game logic
+const cells = document.querySelectorAll('[data-cell]');
+const result = document.querySelector('.result');
+const resetButton = document.getElementById('reset');
+
 let currentPlayer = 'X';
-let result = document.querySelector('.result');
-let btns = document.querySelectorAll('.btn');
-let conditions = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-];
+let gameBoard = ['', '', '', '', '', '', '', '', ''];
+let gameActive = true;
 
-// Function to handle player moves
-const ticTacToe = (element, index) => {
-    // Your game logic here
+// Check for a win or a draw
+function checkResult() {
+  const winningCombinations = [
+      [0, 1, 2],
+          [3, 4, 5],
+              [6, 7, 8],
+                  [0, 3, 6],
+                      [1, 4, 7],
+                          [2, 5, 8],
+                              [0, 4, 8],
+                                  [2, 4, 6],
+                                    ];
 
-    /*
-    **Part 1: Winning Conditions (Add your code here)**
+                                      for (const combination of winningCombinations) {
+                                          const [a, b, c] = combination;
+                                              if (gameBoard[a] && gameBoard[a] === gameBoard[b] && gameBoard[a] === gameBoard[c]) {
+                                                    gameActive = false;
+                                                          return gameBoard[a];
+                                                              }
+                                                                }
 
-    1. Implement the logic to check for winning conditions using the 'conditions' array.
-    2. Display a winning message in the 'result' element when a player wins.
-    3. Disable all buttons after a win.
-    */
+                                                                  if (!gameBoard.includes('')) {
+                                                                      gameActive = false;
+                                                                          return 'draw';
+                                                                            }
 
-    // Your code to update the game state and check for a win
-    // ...
+                                                                              return null;
+                                                                              }
 
-    // Your code to display the current player's turn
-    // ...
+                                                                              // Handle cell click
+                                                                              function handleCellClick(cell, index) {
+                                                                                if (gameBoard[index] === '' && gameActive) {
+                                                                                    gameBoard[index] = currentPlayer;
+                                                                                        cell.textContent = currentPlayer;
+                                                                                            cell.classList.add(currentPlayer);
+                                                                                                
+                                                                                                    const resultValue = checkResult();
+                                                                                                        
+                                                                                                            if (resultValue === 'draw') {
+                                                                                                                  result.textContent = "It's a draw!";
+                                                                                                                      } else if (resultValue) {
+                                                                                                                            result.textContent = `${resultValue} wins!`;
+                                                                                                                                } else {
+                                                                                                                                      currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+                                                                                                                                            result.textContent = `Current player: ${currentPlayer}`;
+                                                                                                                                                }
+                                                                                                                                                  }
+                                                                                                                                                  }
 
-    // Your code to handle button and cell interactions
-    // ...
-};
+                                                                                                                                                  // Handle cell click events
+                                                                                                                                                  cells.forEach((cell, index) => {
+                                                                                                                                                    cell.addEventListener('click', () => {
+                                                                                                                                                        handleCellClick(cell, index);
+                                                                                                                                                          });
+                                                                                                                                                          });
 
-    /*
-    **Part 2: Reset Function (Add your code here)**
+                                                                                                                                                          // Reset the game
+                                                                                                                                                          function resetGame() {
+                                                                                                                                                            gameBoard = ['', '', '', '', '', '', '', '', ''];
+                                                                                                                                                              gameActive = true;
+                                                                                                                                                                currentPlayer = 'X';
+                                                                                                                                                                  cells.forEach((cell) => {
+                                                                                                                                                                      cell.textContent = '';
+                                                                                                                                                                          cell.classList.remove('X', 'O');
+                                                                                                                                                                            });
+                                                                                                                                                                              result.textContent = 'Current player: X';
+                                                                                                                                                                              }
 
-    1. Implement a new function that resets the game to its initial state.
-    2. Ensure the 'cells', 'btns', and 'currentPlayer' variables are reset.
-    3. Update the 'result' element to indicate the current player's turn.
-    4. Re-enable all buttons for a new game.
-    */
-
-// Function to reset the game
-const resetGame = () => {
-    // Your code to reset the game state
-    // ...
-
-    // Your code to update the 'result' element
-    // ...
-
-    // Your code to re-enable buttons
-    // ...
-};
-
-btns.forEach((btn, i) => {
-    btn.addEventListener('click', () => ticTacToe(btn, i));
-});
-
-document.querySelector('#reset').addEventListener('click', resetGame);
+                                                                                                                                                                              resetButton.addEventListener('click', resetGame);
+                                                                                                                                                                              
